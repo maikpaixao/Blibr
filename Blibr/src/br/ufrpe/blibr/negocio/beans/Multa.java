8ad2cb8abf4cb0447e547629d4648892c0d4e437
Maik Luiz Paixão <paixomaik@outlook.com.br>
2017-06-05 16:41:14 -0300

    Atualização das classes principais.

--- Blibr/src/br/ufrpe/blibr/negocio/beans/Multa.java
+++ Blibr/src/br/ufrpe/blibr/negocio/beans/Multa.java
@@ -1,5 +1,36 @@
 package br.ufrpe.blibr.negocio.beans;
 
 public class Multa {
-
+	private double pagamento;
+	private double divida;
+	private double troco;
+	
+	public Multa(double pagamento, double divida){
+		this.pagamento = pagamento;
+		this.divida = divida;
+	}
+	
+	public double getPagamento(){
+		return pagamento;
+	}
+	
+	public void setPagamento(double pagamento){
+		this.pagamento = pagamento;
+	}
+	
+	public double getDivida(){
+		return divida;
+	}
+	
+	public void setDivida(double divida){
+		this.divida = divida;
+	}
+	
+	public double getTroco(){
+		return troco;
+	}
+	
+	public void setTroco(double troco){
+		this.troco = troco;
+	}
 }

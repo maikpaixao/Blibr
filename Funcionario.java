01b18ca2cc25c903380487d2e272b1f3b83ee503
Aluno <Aluno@172.16.204.172>
2017-08-02 16:30:20 -0300

    Removendo arquivos errados.

--- Funcionario.java
+++
@@ -1,8 +0,0 @@
-package br.ufrpe.blibr.negocio.beans;
-
-public class Funcionario extends Pessoa{
-
-	public Funcionario(String nome, String cpf, int idade, String sexo) {
-		super(nome, cpf, idade, sexo);
-	}
-}
